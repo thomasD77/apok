46bf905b5da5fe8a110bdd62e3fb5b15981cd3a8
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-17 16:39:12 +0100

    fav

--- resources/views/includes/thankyou.blade.php
+++ resources/views/includes/thankyou.blade.php
@@ -11,6 +11,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <link rel="icon"
+          type="image/png"
+          href="{{ asset('images/apok_logo.png') }}" />
 </head>
 <body class="body">
 
