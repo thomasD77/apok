14afdc2a53c66e00677d730b0b5deb49086507cc
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-17 16:38:43 +0100

    fav

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,13 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Apok - mini</title>
+        <title>Apok</title>
 
         <!-- Fonts -->
         <link href="http://fonts.cdnfonts.com/css/sofia-pro" rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <link rel="icon"
+              type="image/png"
+              href="{{ asset('images/apok_logo.png') }}" />
     </head>
     <body class="body">
 
