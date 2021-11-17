2beab0125291502e2fbe5bb22414ed63474cf89a
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-11-17 16:29:19 +0100

    fix

--- resources/views/includes/page.blade.php
+++ resources/views/includes/page.blade.php
@@ -11,14 +11,14 @@
                 <h2 class="languages"><a class="languages" href="{{ route('home', 'nl') }}">NL</a><span class="mx-3">-</span><span><a href="{{ route('home', 'fr') }}">FR</a></span></h2>
             </div>
 
-            <div class="col-md-5 header__intro">
+            <div class="col-lg-5 header__intro">
                 <h1>{{__('Goeiedag,')}}</h1>
                 <h2>
                     {{__('Graag jouw informatie over APOK;')}}
                     {{__('Vul enkele vragen in en win mogelijk een exta prijs!')}}
                 </h2>
             </div>
-            <div class="col-md-7 pe-0">
+            <div class="col-lg-7 pe-0">
                 @if(app()->getLocale() == 'nl')
                     <img class="img-fluid header__img d-none d-lg-block" src="{{ asset('images/swirl_man.png') }}" alt="swirl_man">
                 @else
